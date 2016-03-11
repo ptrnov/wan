5fc963e878025837fc4c14780dd130e7cf09ae80
Piter Novian <piter@lipat.co.id>
2016-03-12 00:19:06 +0700

    piter

---
+++ modulprj/master/Modul.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace modulprj\master;
+
+class Modul extends \yii\base\Module
+{
+    public $controllerNamespace = 'modulprj\master\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
