cf8d29fc7d40e5d64d2da24e750546c26eb2ad15
ptrnov <piter@lipat.co.id>
2016-06-15 23:07:44 +0700

    piter

--- modulprj/controllers/ExportController.php
+++ modulprj/controllers/ExportController.php
@@ -71,20 +71,25 @@ class ExportController extends Controller
 	public function actionEmploye(){
 
 		//$custDataMTI=Yii::$app->db_esm->createCommand("CALL ERP_MASTER_CUSTOMER_export('CUSTOMER_MTI')")->queryAll();
-
+						
 		$aryEmp= new ArrayDataProvider([
 			'key' => 'ID',
 			'allModels'=>Yii::$app->db->createCommand("
-							SELECT DISTINCT KAR_ID
-								/* (CASE WHEN x2.GF_ID<>'' THEN (SELECT GF_NM FROM kepangkatan WHERE GF_ID=x2.GF_ID LIMIT 1) ELSE 'none' END) AS GF_NM,
-								(CASE WHEN x2.JOBGRADE_ID<>'' THEN (SELECT JOBGRADE_NM FROM grading WHERE JOBGRADE_ID=x2.JOBGRADE_ID LIMIT 1) ELSE 'none' END) AS GRADING_NM,
+							SELECT DISTINCT KAR_ID,KAR_NM,
 								(CASE WHEN x2.DEP_ID<>'' THEN (SELECT DEP_NM FROM departemen WHERE DEP_ID=x2.DEP_ID LIMIT 1) ELSE 'none' END) AS DEP_NM,
+								 x2.JOBGRADE_ID as Golongan,
+								(CASE WHEN x2.GF_ID<>'' THEN (SELECT GF_NM FROM kepangkatan WHERE GF_ID=x2.GF_ID LIMIT 1) ELSE 'none' END) AS GF_NM,
+								(CASE WHEN x2.JOBGRADE_ID<>'' THEN (SELECT JOBGRADE_NM FROM grading WHERE JOBGRADE_ID=x2.JOBGRADE_ID LIMIT 1) ELSE 'none' END) AS GRADING_NM,								
 								(CASE WHEN x2.CAB_ID<>'' THEN (SELECT CAB_NM FROM cabang WHERE CAB_ID=x2.CAB_ID LIMIT 1) ELSE 'none' END) AS CAB_NM,
-								(CASE WHEN x2.KAR_STS<>'' THEN (SELECT KAR_STS_NM FROM kar_stt WHERE KAR_STS_ID=x2.KAR_STS LIMIT 1) ELSE 'none' END) AS KAR_STS_NM,
-								x2.JOBGRADE_ID as Title,
-								DATE_FORMAT(x2.KAR_TGLM,'%d-%m-%Y') as JoinDate */
-							FROM user x1 RIGHT JOIN karyawan x2 ON x2.KAR_ID=x1.EMP_ID
-							WHERE EMP_ID <>'';
+								(CASE WHEN x2.KAR_STS<>'' THEN (SELECT KAR_STS_NM FROM kar_stt WHERE KAR_STS_ID=x2.KAR_STS LIMIT 1) ELSE 'none' END) AS KAR_STS_NM,								
+								DATE_FORMAT(x2.KAR_TGLM,'%d-%m-%Y') as JoinDate,
+								KAR_KTP,KAR_ALMT,KAR_TLP,KAR_HP,KAR_TGL_LAHIR,
+								(CASE WHEN x2.AGAMA_ID<>'' THEN (SELECT AGAMA_NM FROM agama WHERE AGAMA_ID=x2.AGAMA_ID LIMIT 1) ELSE 'none' END) AS AGAMA,
+								(CASE WHEN x2.STT_ID='K' THEN 'KAWIN' ELSE 'TIDAK KAWIN' END) AS STT_NIKAH,
+								(CASE WHEN x2.KAR_JK='1' THEN 'PRIA' ELSE 'WANITA' END) AS GENDER,
+								(CASE WHEN x2.PEN_ID<>'' THEN (SELECT PEN_NM FROM pendidikan WHERE PEN_ID=x2.PEN_ID LIMIT 1) ELSE 'none' END) AS PEN_NM,
+								JML_ANAK
+							FROM karyawan x2;
 						")->queryAll(),
 			'pagination' => [
 				'pageSize' => 10,
@@ -99,13 +104,34 @@ class ExportController extends Controller
         $excel_ceils = $excel_data['excel_ceils'];
 		$excel_content = [
 			 [
-				'sheet_name' => 'Data Employee',
-                'sheet_title' => ['KAR_ID'],
+				'sheet_name' => 'Employee',
+                'sheet_title' => [
+					'KAR_ID','KAR_NM','DEPARTMENT','GOLONGAN','LEVEL','GRADING','CABANG','STATUS KERJA','TANGGAL MASUK','KTP','ALAMAT',
+					'TLP','HP','TGL LAHIR','AGAMA','STATUS PERNIKAHAN','JENIS KELAMIN','PENDIDIKAN','JUMLAH ANAK'
+				],
 			    'ceils' => $excel_ceils,
                 //'freezePane' => 'E2',
                 'headerColor' => Export2ExcelBehavior::getCssClass("header"),
                 'headerColumnCssClass' => [
-					 'KAR_ID' => Export2ExcelBehavior::getCssClass('header')              
+					 'KAR_ID' => Export2ExcelBehavior::getCssClass('header'),
+					 'KAR_NM' => Export2ExcelBehavior::getCssClass('header'),
+					 'DEP_NM' => Export2ExcelBehavior::getCssClass('header'),
+					 'Golongan' => Export2ExcelBehavior::getCssClass('header'),
+					 'GF_NM' => Export2ExcelBehavior::getCssClass('header'),
+					 'GRADING_NM' => Export2ExcelBehavior::getCssClass('header'),
+					 'CAB_NM' => Export2ExcelBehavior::getCssClass('header'),
+					 'KAR_STS_NM' => Export2ExcelBehavior::getCssClass('header'),
+					 'JoinDate' => Export2ExcelBehavior::getCssClass('header'),					 
+					 'KAR_KTP' => Export2ExcelBehavior::getCssClass('header'),					 
+					 'KAR_ALMT' => Export2ExcelBehavior::getCssClass('header'),					 
+					 'KAR_TLP' => Export2ExcelBehavior::getCssClass('header'),					 
+					 'KAR_HP' => Export2ExcelBehavior::getCssClass('header'),					 
+					 'KAR_TGL_LAHIR' => Export2ExcelBehavior::getCssClass('header'),					 
+					 'AGAMA' => Export2ExcelBehavior::getCssClass('header'),					 
+					 'STT_NIKAH' => Export2ExcelBehavior::getCssClass('header'),					 
+					 'GENDER' => Export2ExcelBehavior::getCssClass('header'),					 
+					 'KAR_PEN' => Export2ExcelBehavior::getCssClass('header'),					 
+					 'JML_ANAK' => Export2ExcelBehavior::getCssClass('header'),					 
                 ], //define each column's cssClass for header line only.  You can set as blank.
                'oddCssClass' => Export2ExcelBehavior::getCssClass("odd"),
                'evenCssClass' => Export2ExcelBehavior::getCssClass("even"),
