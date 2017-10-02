3a3cfe2cc08a7d5e650af478c00541c5dba0286a
ptrnov <ptr.nov@gmail.com>
2017-10-02 22:45:47 +0700

    update

--- modulprj/payroll/controllers/AbsenDailyController.php
+++ modulprj/payroll/controllers/AbsenDailyController.php
@@ -62,6 +62,30 @@ class AbsenDailyController extends Controller
         ];
     }
 
+	/**
+     * Before Action Index
+     */
+	public function beforeAction(){
+			if (Yii::$app->user->isGuest)  {
+				 Yii::$app->user->logout();
+                   $this->redirect(array('/site/login'));  //
+			}
+            // Check only when the user is logged in
+            if (!Yii::$app->user->isGuest)  {
+               if (Yii::$app->session['userSessionTimeout']< time() ) {
+                   // timeout
+                   Yii::$app->user->logout();
+                   $this->redirect(array('/site/login'));  //
+               } else {
+                   //Yii::$app->user->setState('userSessionTimeout', time() + Yii::app()->params['sessionTimeoutSeconds']) ;
+				   Yii::$app->session->set('userSessionTimeout', time() + Yii::$app->params['sessionTimeoutSeconds']);
+                   return true; 
+               }
+            } else {
+                return true;
+            }
+    }
+	
     /**
      * Lists all AbsenImport models.
      * @return mixed
